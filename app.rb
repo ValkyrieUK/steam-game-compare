068cb0f6f2f7586b67666cb76b6ea43e1cc23c2a
Bradley Allen <valkyrieuk@Bradley-Allens-Mac.local>
2014-05-21 13:34:50 +0100

    compare all the things

--- app.rb
+++ app.rb
@@ -10,7 +10,12 @@ post '/compare' do
   puts params[:steam_id2]
   @person1_games = steam_client.get_games(params[:steam_id1])
   @person2_games = steam_client.get_games(params[:steam_id2])
+  @games_in_common = games_in_common
   erb :compare
+end
+
+def games_in_common
+  @person1_games & @person2_games
 end
 
 def steam_client
